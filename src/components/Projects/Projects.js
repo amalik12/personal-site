5bab0f19a331ef382c1c631227fdc3302df37078
Adisa Malik <adisamalik@gmail.com>
2019-01-20 21:01:57 -0500

    Added site content

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Projects.css';
 import ProjectItem from '../ProjectItem/';
 import { revealOptions } from '../../util';
+import { projects } from '../../content';
 import ScrollReveal from 'scrollreveal';
 
 class Projects extends Component {
@@ -24,15 +25,10 @@ class Projects extends Component {
     render() {
         return (
             <div className="Projects">
-                <ProjectItem ref={node => this.nodes.push(node)} large={true} title="Groop" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis."
-                tags={['react', 'mongodb', 'nodejs', 'redis']}
-                github="https://github.com/amalik12/groop" image="groop.gif"/>
-                <ProjectItem ref={node => this.nodes.push(node)} right={true} large={true} title="Draftnet" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis."
-                tags={['django', 'tensorflow', 'angular']}
-                github="https://github.com/viking-sudo-rm/draftnet/" image="draftnet.png"/>
-                <ProjectItem ref={node => this.nodes.push(node)} large={true} title="Dining Plus" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis."
-                tags={['android', 'java', 'sqlite']}
-                github="https://github.com/amalik12/dining_plus" image="diningplus.png"/>
+                {projects.map((item, index) => {
+                    return <ProjectItem key={index} ref={node => this.nodes.push(node)} large={true}
+                    right={index % 2 === 1} {...item} />
+                })}
             </div>
         );
     }
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -3,6 +3,7 @@ import ScrollReveal from 'scrollreveal';
 import './Resume.css';
 import Experience from '../Experience';
 import { revealOptions } from '../../util';
+import { resume } from '../../content';
 
 
 class Resume extends Component {
@@ -34,21 +35,7 @@ class Resume extends Component {
             <div className="header" ref={node => this.nodes.push(node)}>
                 <h2 className="header-text">Experience.</h2><span className="divider"></span>
             </div>
-            <Experience ref={node => this.nodes.push(node)} image="bloomberg.png" title="Software Engineer"
-            org={['Bloomberg', 'http://bloomberg.com']} dates="Sept 2018 - Present"
-            location="New York, NY" tags={['c++', 'python']} description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.'/>
-            <Experience ref={node => this.nodes.push(node)} image="datadog.png" title="Solutions Engineer"
-            org={['Datadog', 'http://datadoghq.com']} dates="Jan 2018 - June 2018"
-            location="New York, NY" tags={['python', 'javascript']} description=''/>
-            <Experience ref={node => this.nodes.push(node)} image="bi.png" title="Front End Engineering Intern"
-            org={['Business Insider', 'http://businessinsider.com']} dates="June 2017 - Aug 2017"
-            location="New York, NY" tags={['javascript', 'php', 'angularjs']} description=''/>
-            <Experience ref={node => this.nodes.push(node)} image="yale.png" title="Undergraduate Learning Assistant"
-            org={['Yale University', 'http://yale.edu']} dates="Sept 2016 - Dec 2016"
-            location="New Haven, CT" tags={['c']} description=''/>
-            <Experience ref={node => this.nodes.push(node)} image="socrob.png" title="Research Intern"
-            org={['Yale Social Robotics Lab', 'http://scazlab.yale.edu']} dates="June 2016 - Aug 2017"
-            location="New Haven, CT" tags={['python', 'opencv']} description=''/>
+            {resume.map((item, index) => <Experience key={index} ref={node => this.nodes.push(node)} {...item}/>)}
             <div ref={node => this.nodes.push(node)} className="resume-download"><i className="fas fa-file-pdf"></i> <span className="resume-download-text">Download Resume</span></div>
           </div>
         );
